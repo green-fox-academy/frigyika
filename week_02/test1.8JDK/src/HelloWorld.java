fcecdfbc88dc5efc5a1e7724e7223430d6431cef
Andras Frigyik <terhes12@gmail.com>
2021-10-19 14:11:26 +0200

    Java_feladat_1_feltöltés

---
+++ week_02/test1.8JDK/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    //pvsm utasítás létrehozza a public static void...kifejezést
+    public static void main(String[] args) {
+        //sout
+        System.out.println("Hello World!");
+        
+    }
+}
