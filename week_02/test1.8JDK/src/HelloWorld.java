80ddd457adaee3259768c10767adbc101ca4fe7e
Andras Frigyik <65549450+frigyika@users.noreply.github.com>
2021-10-19 19:01:02 +0200

    Delete week_02 directory

--- week_02/test1.8JDK/src/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-public class HelloWorld {
-    //pvsm utasítás létrehozza a public static void...kifejezést
-    public static void main(String[] args) {
-        //sout
-        System.out.println("Hello World!");
-        
-    }
-}
